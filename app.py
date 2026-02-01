2700d5f45ed73719e7dc463430dd9fe8d9f3978a
Knalini-94 <nalinikumari9494@gmail.com>
2026-02-02 00:07:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/love")
+def love():
+    return render_template("love.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
